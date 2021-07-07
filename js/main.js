5ac530b32a24cfc0eab56a7356832a3a4471d4b3
Eugene Kartashian <e.kartashian@gmail.com>
2021-07-07 12:24:41 +0300

    Add files via upload
    added preloader

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
+window.onload = function () {
+        document.body.classList.add('loaded_hiding');
+        window.setTimeout(function () {
+        document.body.classList.add('loaded');
+        document.body.classList.remove('loaded_hiding');
+    }, 500);
+}
+
 $(document).ready(function(){
 	$('.gallery_content').createDiagonalSlider();
 });
